662fdc8490a01b558050353e33a22f42190122f4
Novitasarinapitupulu <novitasarinapitupulu35@gmail.com>
2025-11-18 21:22:27 +0700

    28
    selesai

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,28 @@
-//Nama - NIM
+//novitasari napitupuluu -12s25028
 
+
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String command;
+        String[] deskripsi = new String[10];
+        String[] kode = new String[10];
+        String[] matkul = new String[10];
+        String[] dosen = new String[10];
+        String[] dendline = new String[10];
+        String[] ketID = new String[10];
+        String[] kesulitan = new String[10];
+        String[] hari = new String[10];
+
+        totaldata = 0;
+        while (command.equals("---")) {
+            command = input.nextLine();
+            if (command.equals("update task status")) {
+            }
+        }
+    }
+}
